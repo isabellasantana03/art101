365fb9a32eec0ca673bb0d63b901dc6639d9c4d6
isabellasantana03 <imsantan@ucsc.edu>
2024-05-09 16:20:33 -0700

    update

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -9,9 +9,31 @@
 
 //add button to challenge section
 $("#challenge").append("<button id='button-challenge'>Make Special</button>");
-
 //add a click listener to the challenge button 
 $("button-challenge").click(function(){
     //now add (or subtract) the "special" class to the section
     $("#challenge").toggleClass("special");
 });
+
+//add button to problems section
+$("#problems").append("<button id='button-problems'>Make Special</button>");
+//add a click listener to the problems button 
+$("button-problems").click(function(){
+    //now add (or subtract) the "special" class to the section
+    $("#problems").toggleClass("special");
+});
+
+//add button to results section
+$("#results").append("<button id='button-results'>Make Special</button>");
+//add a click listener to the results button 
+$("button-results").click(function(){
+    //now add (or subtract) the "special" class to the section
+    $("#results").toggleClass("special");
+});
+
+//bonus task 
+$('.special-section').append("<button>Make Special<button>")
+.find("button").click(function(){
+    $(this).parent().toggleClass('special')
+});
+
