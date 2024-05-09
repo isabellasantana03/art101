b4a4d678adf823d96a01c5e3465e10b001fded9f
isabellasantana03 <imsantan@ucsc.edu>
2024-05-09 14:52:21 -0700

    update

---
+++ lab9/js/lab.js
@@ -0,0 +1,12 @@
+/*
+    lab.js -This simple JS/jQuery script uses buttons to modify some elements on the page
+
+    Requirements: jQuery must be loaded for this script to work. 
+
+    Author: Isabella Santana 
+    Date: May 9, 2024
+*/
+
+//add button to challenge section
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+
