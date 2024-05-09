a7718e37adb8892e77053af5ca578af98858582e
isabellasantana03 <imsantan@ucsc.edu>
2024-05-09 15:17:49 -0700

    update

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -10,3 +10,8 @@
 //add button to challenge section
 $("#challenge").append("<button id='button-challenge'>Make Special</button>");
 
+//add a click listener to the challenge button 
+$("button-challenge").click(function(){
+    //now add (or subtract) the "special" class to the section
+    $("#challenge").toggleClass("special");
+});
