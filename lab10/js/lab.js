e59a58d2b31cfb8d177afb20f76742ddc10fbbfb
isabellasantana03 <imsantan@ucsc.edu>
2024-05-15 11:31:49 -0700

    update

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,18 +1,28 @@
-.minor-section {
-    padding: 10px;
-    margin-bottom: 10px;
-    border: solid 1px #a386e6;
-    background-color: #a386e6;
+/*
+    lab.js - Thissimple JS/jQuery script appends new elements to an output div
+    Requirements: jQuery must be loaded for this script to work.
+
+Author: Isabella Santana 
+Date: May 15, 2024
+*/
+
+function generateRandomText() {
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    const min = 3;
+    const max = 100;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    // Get a random starting index to slice the Lorem Ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    // Generate the random Lorem Ipsum-like text
+    return text.slice(randStart, randStart + randLen);
   }
 
-text p {
-    display: inline-block;
-    max-width: 60%;
-    margin: 3px;
-    padding: 5px 10px;
-    border-radius: 15px;
-    background-color: darkgray;
-    color: white;
-    font-family: sans-serif;
-    font-size: 0.8em;
-  }
+//click listener for button 
+$("#make-convo").click(function(){
+});
+
+//get new fake dialogue
+const newText=generateRandomText();
+
+//append a new div to our output div 
+$("#output").append('<div class="text"><p>'+ newText + '</p></div>');
