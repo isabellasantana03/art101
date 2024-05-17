a4514564dc6f22c26e2cd5ec792a42cb8b6f0688
isabellasantana03 <imsantan@ucsc.edu>
2024-05-16 17:54:14 -0700

    update

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,10 +1,7 @@
-/*
-    lab.js - This simple JS/jQuery script appends new elements to an output div
-    Requirements: jQuery must be loaded for this script to work.
-
-Author: Isabella Santana 
-Date: May 15, 2024
-*/
+// lab.js - This simple JS/jQuery script appends new elements to an output div
+// Requirements: jQuery must be loaded for this script to work.
+// Author: Isabella Santana 
+// Date: May 15, 2024
 
 function generateRandomText() {
     const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
@@ -15,18 +12,15 @@ function generateRandomText() {
     const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
     // Generate the random Lorem Ipsum-like text
     return text.slice(randStart, randStart + randLen);
-  }
+}
 
-$(document).ready(function(){
-    $("#make_convo").click(function() {
+$(document).ready(function() {
+    $("#make-convo").click(function() {
         const newText = generateRandomText();
-        const outputDivs = $("#output.text");
+        const outputDivs = $("#output .text");
         const newClass = (outputDivs.length % 2 === 0) ? 'text-left' : 'text-right';
-
-         // Inline styles for debugging
-         const inlineStyle = (newClass === 'text-left') ? 'background-color: #4b4848; align-self: flex-start; color: white;' : 'background-color: #90ee90; align-self: flex-end; color: black;';
-        
-        $("#output").append('<div class = "text ' + newClass + '"><p>' + newText + '</p></div>');
+        $("#output").append('<div class="text ' + newClass + '"><p>' + newText + '</p></div>');
     });
 });
+
 
