5ed5b2bf4ae13d382925c2a09c442c341aff67ae
isabellasantana03 <imsantan@ucsc.edu>
2024-05-19 17:34:40 -0700

    update

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -21,3 +21,49 @@ $(document).ready(function() {
     });
 });
 
+
+$(document).ready(function() {
+    // Existing code...
+
+   // Event listener for changing text color
+   $("#change-color").click(function() {
+    $(".text").css("color", getRandomColor());
+});
+  // Event listener for glitching the page
+  $("#glitch").click(function() {
+    glitchPage();
+});
+// Helper function to get a random color
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Function to glitch the page
+function glitchPage() {
+    $(".text").each(function() {
+        const top = Math.random() * 100;
+        const left = Math.random() * 100;
+        $(this).css({
+            position: 'relative',
+            top: top + 'px',
+            left: left + 'px'
+        });
+    });
+
+    setTimeout(resetGlitch, 1000); // Reset glitch after 1 second
+}
+
+// Function to reset the glitch effect
+function resetGlitch() {
+    $(".text").css({
+        position: 'static',
+        top: '0px',
+        left: '0px'
+    });
+}
+});
